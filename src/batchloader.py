4ec6b22ff96f4b3d4062a096c7628d0cc160d33e
Julia Belloni <juliabelloni@RN-145-90-162-14.eduroam.rug.nl>
2024-01-15 13:24:04 +0100

    batchloader implementation

---
+++ src/batchloader.py
@@ -0,0 +1,36 @@
+
+
+class BatchLoader():
+    def __init__(self):
+        self._batch_size = None
+        self._fashion = None
+        self._data = None
+        # train data is a dataset considering that strategy has a getter
+
+    def _single_batch_load(self):
+        for i in range(self._batch_size):
+                data = self._data[i:i + self._batch_size]
+            pass
+    
+    def load_batches(self, train_data, batch_size, fashion):
+        self._batch_size = batch_size
+        self._fashion = fashion
+        self._data = train_data
+        if len(train_data)%batch_size == 0:
+            self._single_batch_load()
+        else:
+            #ask user if they want to use last batch of the train
+            print("the last batch is smaller, type yes if you still want to use the last batch, type no otherwise?")
+            user = input()
+            if user == "yes":
+                self._single_batch_load
+            elif user == "no":
+                for i in range(self._batch_size):
+                    if i == self._batch_size - 1:
+                        data = self._data[i:]
+                    data = self._data[i:i + self._batch_size]
+            pass
+
+    def __len__(self):
+        pass
+                 
