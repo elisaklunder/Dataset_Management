8aad404f14b3ee49ff2230ce12be56ad496c533f
Julia Belloni <juliabelloni@Julias-MacBook-Air.local>
2024-01-15 20:32:34 +0100

    some changes to the batchloader, but overall happy part1 works

--- src/batchloader.py
+++ src/batchloader.py
@@ -15,7 +15,7 @@ class BatchLoader():
     def load_batches(self, train_data, batch_size, fashion):
         self._batch_size = batch_size
         self._fashion = fashion
-        self._data = train_data
+        self._data = train_data.data
         if len(train_data)%batch_size == 0:
             self._single_batch_load()
         else:
@@ -23,14 +23,17 @@ class BatchLoader():
             print("the last batch is smaller, type yes if you still want to use the last batch, type no otherwise?")
             user = input()
             if user == "yes":
-                self._single_batch_load
-            elif user == "no":
-                for i in range(self._batch_size):
+                for i in range(self._batch_size -1):
+                    #case for the last batch
                     if i == self._batch_size - 1:
                         data = self._data[i:]
+                    self._single_batch_load
+            elif user == "no":
+                for i in range(self._batch_size -1):
                     data = self._data[i:i + self._batch_size]
             pass
 
     def __len__(self):
-        pass
+        print(f"number of batches that can be created using {self._batch_size} as batch size")
+        return self._data//self._batch_size
                  
