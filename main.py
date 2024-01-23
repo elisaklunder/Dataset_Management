be2d392f35356b3759585539b89641f4ff0793a2
Julia Belloni <juliabelloni@Julias-Air.home>
2024-01-23 10:04:12 +0100

    some changes

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ def main():
     #root_path = r"C:\Users\elikl\Documents\Università\yr2\2 - OOP\oop-final-project-group-7\classification_csv\images_poly"
     #root_path = r"C:\Users\elikl\Documents\Università\yr2\2 - OOP\oop-final-project-group-7\classification_hierarchy"
     #labels_path = r"C:\Users\elikl\Documents\Università\yr2\2 - OOP\oop-final-project-group-7\classification_csv\poly_targets_classification.csv"
-    image.load_data(root_path, "lazy", "csv", labels_path)
+    image.load_data(root_path, "eager", "csv")
     train, test = image.train_test_split(
         train_size=0.6, test_size=0.4, shuffle=False
     )
    
-    print(type(train[3]))
-    print(train.targets[1])
-    train.targets[1] = 435345
-    print(train.targets[1])
+    print((train[3]))
+    #print(train.targets[1])
+    #train.targets[1] = 435345
+    #print(train.targets[1])
 
     
     batcher = BatchLoader()
