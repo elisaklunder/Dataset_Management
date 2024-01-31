53b5223ff46d86e959d4b65bac3a42fbbc31fa5b
Julia Belloni <juliabelloni@Julias-Air.home>
2024-01-31 21:58:33 +0100

    padding is working

--- src/image_padding.py
+++ src/image_padding.py
@@ -7,6 +7,8 @@ import numpy as np
 
 class ImagePatching(PreprocessingTechniqueABC):
     def __init__(self, color, height, width):
+        # implement error if the box is bigger than the image
+        #handle everything  when crop is too big
         self._color = color
         self._width = width
         self._height = height
@@ -18,36 +20,23 @@ class ImagePatching(PreprocessingTechniqueABC):
         x_left = random.randint(0, width - self._width)
         y_left = random.randint(0, height - self._height)
 
-        x_right = x_left + width
-        y_right = y_left + height
+        x_right = x_left + self._width
+        y_right = y_left + self._height
         box = (x_left, y_left, x_right, y_right)
+        # box_image = image.crop(box)
         region = Image.new("RGB", (self._width, self._height), self._color)
+        
         image.paste(region, box)
-        image.show()
         image_array = np.array(image)
 
         return image_array
-
-    #this works if the image is kept as an array
-    def _patchingArray(self, image):
-        width, height = image.size(image)
-        x_left = random.randint(0, self._width - width)
-        y_left = random.random(0, self.heigh - height)
-        x_right = x_left + width
-
-        rgb = matplotlib.colors.to_rgb(self._color)
-
-        for i in range(height):
-            image[y_left + i, x_left:x_right] = rgb
-
-        return image
 
     def __call__(self, image):
         return self._patching(image)
 
 
 if __name__ == "__main__":
-    preproces = ImagePatching("blue", 14, 14)
+    preproces = ImagePatching("yellow", 155, 145)
     path = "/Users/juliabelloni/Desktop/oop/assignments/oop-final-project-group-7/image_classification_hierarchy/Cat/0a0da090aa9f0342444a7df4dc250c66.jpg"
     image = Image.open(path)
     new_image = np.array(image)
