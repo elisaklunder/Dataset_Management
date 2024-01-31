8261fbe0e2a0810b664e287e833cd0d0a9f3c826
Julia Belloni <juliabelloni@Julias-Air.home>
2024-01-31 21:11:53 +0100

    preprocessing

--- src/image_loader.py
+++ src/image_loader.py
@@ -7,15 +7,13 @@ from PIL import Image
 class ImageLoader:
     def _read_data_file(self, path):
         try:
-            image_d = Image.open(path)
-            image_d.load()
-            image_d.show()
+            image = Image.open(path)
         except Exception as e:
             print(
                 f"Error opening image {os.path.basename(path)}: {e}"
             )  # specify the error better
-        #image_data = np.array(image)
-        return image_d
+        image_data = np.array(image)
+        return image_data
 
     def __call__(self, path):
         return self._read_data_file(path)
--- src/image_padding.py
+++ src/image_padding.py
@@ -2,6 +2,7 @@ from PIL import Image
 from preprocessing_ABC import PreprocessingTechniqueABC
 import random
 import matplotlib
+import numpy as np
 
 
 class ImagePatching(PreprocessingTechniqueABC):
@@ -10,7 +11,8 @@ class ImagePatching(PreprocessingTechniqueABC):
         self._width = width
         self._height = height
 
-    def _patching(self, image):
+    def _patching(self, array):
+        image = Image.fromarray(array)
         width, height = image.size(image)
         x_left = random.randint(0, self._width - width)
         y_left = random.random(0, self.heigh - height)
@@ -22,9 +24,12 @@ class ImagePatching(PreprocessingTechniqueABC):
         region = image.crop(box)
         region = Image.new("RGB", (self._width, self._height), self._color)
         image.paste(region, box)
+        image.show()
+        image_array = np.array(image)
 
-        return image
+        return image_array
 
+    #this works if the image is kept as an array
     def _patchingArray(self, image):
         width, height = image.size(image)
         x_left = random.randint(0, self._width - width)
@@ -34,9 +39,16 @@ class ImagePatching(PreprocessingTechniqueABC):
         rgb = matplotlib.colors.to_rgb(self._color)
 
         for i in range(height):
-            image[y_left + i, x_left:x_right] = rgb = rgb
+            image[y_left + i, x_left:x_right] = rgb
 
         return image
 
     def __call__(self, image):
         return self._patching(image)
+
+if __name__ == "__main__":
+    preproces = ImagePatching()
+    path = "/Users/juliabelloni/Desktop/oop/assignments/oop-final-project-group-7/image_classification_hierarchy/Cat/0a0da090aa9f0342444a7df4dc250c66.jpg"
+    image = Image.open(path)
+    new_image = np.array(image)
+
