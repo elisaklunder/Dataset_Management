95a2ad68b86bbd361237010f45468141b3a36c55
Julia Belloni <juliabelloni@Julias-Air.home>
2024-01-31 21:38:33 +0100

    sjvb

--- src/image_padding.py
+++ src/image_padding.py
@@ -12,16 +12,15 @@ class ImagePatching(PreprocessingTechniqueABC):
         self._height = height
 
     def _patching(self, array):
+        
         image = Image.fromarray(array)
-        width, height = image.size(image)
-        x_left = random.randint(0, self._width - width)
-        y_left = random.random(0, self.heigh - height)
+        width, height = image.size
+        x_left = random.randint(0, width - self._width)
+        y_left = random.randint(0, height - self._height)
 
         x_right = x_left + width
-        y_right = y_left - height
-
+        y_right = y_left + height
         box = (x_left, y_left, x_right, y_right)
-        region = image.crop(box)
         region = Image.new("RGB", (self._width, self._height), self._color)
         image.paste(region, box)
         image.show()
@@ -46,9 +45,11 @@ class ImagePatching(PreprocessingTechniqueABC):
     def __call__(self, image):
         return self._patching(image)
 
+
 if __name__ == "__main__":
-    preproces = ImagePatching()
+    preproces = ImagePatching("blue", 14, 14)
     path = "/Users/juliabelloni/Desktop/oop/assignments/oop-final-project-group-7/image_classification_hierarchy/Cat/0a0da090aa9f0342444a7df4dc250c66.jpg"
     image = Image.open(path)
     new_image = np.array(image)
+    preproces(new_image)
 
