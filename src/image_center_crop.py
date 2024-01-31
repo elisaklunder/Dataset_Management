43aea08d2b852b8d51902b16e26078b59113957c
Elisa Klunder <elisaklunder@hotmail.com>
2024-01-31 21:57:08 +0100

    image crop

--- src/image_center_crop.py
+++ src/image_center_crop.py
@@ -7,13 +7,10 @@ from preprocessing_ABC import PreprocessingTechniqueABC
 class ImageCenterCrop(PreprocessingTechniqueABC):
     def __init__(self, width, height):
         self._width = width
-        self.height = height
+        self._height = height
 
-    def __call__(self, dataset):
-        return self._dataset_processing(dataset)
-
-    def _crop_image(self, image):
-        H, W = np.shape(image)
+    def _preprocessing_logic(self, data):
+        H, W, _ = np.shape(image)
         H_center = H // 2
         W_center = W // 2
         top = max(H_center - self._height // 2, 0)
@@ -23,20 +20,15 @@ class ImageCenterCrop(PreprocessingTechniqueABC):
         cropped_image = image[top:bottom, left:right]
         return cropped_image
 
-    def _dataset_processing(self, data):
-        if isinstance(data, tuple):
-            image, target = data
-            cropped_image = self._crop_image(image)
-            return cropped_image, target
-        else:
-            cropped_image = self._crop_image(data)
-            return cropped_image
+
 
 
 if __name__ == "__main__":
     crop = ImageCenterCrop(100, 100)
     path = r"C:\Users\elikl\Documents\Università\yr2\2 - OOP\oop-final-project-group-7\image_regression_csv\images_poly\000cf421-6725-4dee-bf37-04525ba04340.png"
     image = Image.open(path)
+    image.show()
     image = np.array(image)
     cropped_image = crop(image)
-    print(np.shape(cropped_image))
+    showoff = Image.fromarray(cropped_image)
+    showoff.show()
--- src/preprocessing_ABC.py
+++ src/preprocessing_ABC.py
@@ -2,4 +2,17 @@ from abc import ABC
 
 
 class PreprocessingTechniqueABC(ABC):
-    pass
+    def __call__(self, data):
+        return self._preprocessing(data)
+
+    def _preprocessing_logic(self, data):
+        pass
+
+    def _preprocessing(self, data_point):
+        if isinstance(data_point, tuple):
+            data, target = data_point
+            data = self._preprocessing_logic(data)
+            return data, target
+        else:
+            data = self._preprocessing_logic(data)
+            return data
