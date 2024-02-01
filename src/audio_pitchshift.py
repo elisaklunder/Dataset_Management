6075349259cda86d74459d9bdb3bb307b060bb07
Julia Belloni <juliabelloni@Julias-Air.home>
2024-02-01 19:21:09 +0100

    sfjworh

--- src/audio_pitchshift.py
+++ src/audio_pitchshift.py
@@ -2,24 +2,29 @@ import librosa
 from preprocessing_ABC import PreprocessingTechniqueABC
 from errors import Errors
 
+
 class PitchShifting(PreprocessingTechniqueABC):
     def __init__(self, pitch_shift):
-        error = Errors()
-        error.type_check("pitch_shift", pitch_shift, int)
-        error.ispositive("pitch_shift", pitch_shift)
+        self._error = Errors()
+        self._error.type_check("pitch_shift", pitch_shift, int)
+        self._error.ispositive("pitch_shift", pitch_shift)
         self._pitch_shift = pitch_shift
 
     def _preprocessing_logic(self, audio):
         sample, sampling_rate = audio
-        shifted_audio = librosa.effects.pitch_shift(
+        try:
+            shifted_audio = librosa.effects.pitch_shift(
             sample, sr=sampling_rate, n_steps=self._pitch_shift,bins_per_octave=self._pitch_shift
             )
+        except librosa.util.exceptions.ParameterError:
+            raise ValueError("Audio data is in the wrong format")
         return shifted_audio, sampling_rate
     
 
 if __name__ == "__main__":
-    preproces = PitchShifting(0)
-    path = "/Users/juliabelloni/Desktop/oop/assignments/oop-final-project-group-7/audio_classification_hierarchy/Cats/cat0548.wav"
+    preproces = PitchShifting(45)
+    #path = "/Users/juliabelloni/Desktop/oop/assignments/oop-final-project-group-7/audio_classification_hierarchy/Cats/cat0548.wav"
+    path = "/Users/juliabelloni/Desktop/oop/assignments/oop-final-project-group-7/image_classification_hierarchy/Dog/0a3d571cec18903dd963639dddf5587d.jpg"
     audio = librosa.load(path)
     new_audio = preproces(audio)
     print(len(new_audio[0]), len(audio[0]))
--- src/audio_resampling.py
+++ src/audio_resampling.py
@@ -10,9 +10,12 @@ class Resampling(PreprocessingTechniqueABC):
         error.ispositive("new_sampling_rate", new_sampling_rate)
         self._new_sampling_rate = new_sampling_rate
 
-    def _preprocessing_logic(self, audio):
+    def _preprocessing_logic(self, audio):   
         samples, sampling_rate = audio
-        resampled_signal = librosa.resample(samples, orig_sr=sampling_rate, target_sr=self._new_sampling_rate)
+        try:
+            resampled_signal = librosa.resample(samples, orig_sr=sampling_rate, target_sr=self._new_sampling_rate)
+        except librosa.util.exceptions.ParameterError:
+            raise ValueError("Audio data is in the wrong format")
         return resampled_signal, self._new_sampling_rate
 
 
@@ -20,6 +23,4 @@ if __name__ == "__main__":
     preproces = Resampling(875)
     path = "/Users/juliabelloni/Desktop/oop/assignments/oop-final-project-group-7/audio_classification_hierarchy/Cats/cat0548.wav"
     audio = librosa.load(path)
-    print(audio)
     new_audio = preproces(audio)
-    print(new_audio)
