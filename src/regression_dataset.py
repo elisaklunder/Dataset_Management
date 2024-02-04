a405c2f0b60ee7c2a97fab6a4bca751228bf2c7a
Elisa Klunder <elisaklunder@hotmail.com>
2024-02-04 14:55:08 +0100

    imn

--- src/regression_dataset.py
+++ src/regression_dataset.py
@@ -5,4 +5,23 @@ class RegressionDataset(BaseDataset):
     """
     Class implementing regression datasets.
     """
-    pass
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._wrong_format_error()
+
+    def _wrong_format_error(self):
+        if self._format == "hierarchical":
+            raise ValueError(
+                "A regression dataset can't be organized in\
+hierarchical structure"
+            )
+
+
+def main():
+    dataset = RegressionDataset()
+    dataset.load_data(root="blabla", strategy="lazy", format="hierarchical")
+
+
+if __name__ == "__main__":
+    main()
